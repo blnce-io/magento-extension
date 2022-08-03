500d80a1328261d9827f6f25fdca9f801e961969
Dhiraj Deore <dhirajkumar.deore@toptal.com>
2022-08-03 13:17:44 +0530

    BR-2807 - Adding notice for offline credit memo and refund

---
+++ ViewModel/InvoiceForm.php
@@ -0,0 +1,31 @@
+<?php
+namespace Balancepay\Balancepay\ViewModel;
+
+use Balancepay\Balancepay\Model\ResourceModel\BalancepayCharge\Collection;
+use Magento\Framework\App\RequestInterface;
+use Magento\Framework\View\Element\Block\ArgumentInterface;
+
+class InvoiceForm implements ArgumentInterface
+{
+    public function __construct(
+        Collection $collection,
+        RequestInterface $request
+    ) {
+        $this->collection = $collection;
+        $this->request = $request;
+    }
+
+    public function enableCreditMemo() {
+
+        $invoiceId = $this->request->getParam('invoice_id');
+        if(!$invoiceId) {
+            return true;
+        }
+        $chargeId = $this->collection->addFieldToFilter('invoice_id', ['eq' => $invoiceId])
+            ->getFirstItem()->getChargeId();
+        if (!$chargeId) {
+            return false;
+        }
+        return true;
+    }
+}
