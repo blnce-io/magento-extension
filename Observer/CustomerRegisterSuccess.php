2f64287b6a2ec2a32ac416cb30a4b4e43b8197c8
Dhirajkumar Deore <dhirajkumar.41@gmail.com>
2022-08-23 13:13:15 +0530

    BR-2350 - Updating logic for buyer save

--- Observer/CustomerRegisterSuccess.php
+++ Observer/CustomerRegisterSuccess.php
@@ -6,24 +6,44 @@ use Magento\Framework\Event\ObserverInterface;
 use Magento\Framework\Message\ManagerInterface;
 use Magento\Framework\Session\SessionManagerInterface;
 use Balancepay\Balancepay\Model\BalanceBuyer;
+use Balancepay\Balancepay\Model\Config;
 
 class CustomerRegisterSuccess implements ObserverInterface
 {
+    /**
+     * @var SessionManagerInterface
+     */
+    protected $_coreSession;
+
+    /**
+     * @var BalanceBuyer
+     */
+    protected $balanceBuyer;
+
+    /**
+     * @var Config
+     */
+    protected $balancepayConfig;
+
     public function __construct(
         SessionManagerInterface $coreSession,
-        BalanceBuyer $balanceBuyer
+        BalanceBuyer $balanceBuyer,
+        Config $balancepayConfig
     ) {
         $this->_coreSession = $coreSession;
         $this->balanceBuyer = $balanceBuyer;
+        $this->balancepayConfig = $balancepayConfig;
     }
 
     public function execute(Observer $observer)
     {
         try {
             $buyerId = $this->_coreSession->getBalanceBuyerId();
-            $this->balanceBuyer->updateCustomerBalanceBuyerId($buyerId);
+            if ($buyerId) {
+                $this->balanceBuyer->updateCustomerBalanceBuyerId($buyerId);
+            }
         } catch (\Exception $e) {
-
+            $this->balancepayConfig->log('Customer Register Success - Couldnot assign the buyer to customer');
         }
     }
 }
