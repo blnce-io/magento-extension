39567855eb80237528f01d64bcd906c2aab6b68e
Dhirajkumar Deore <dhirajkumar.41@gmail.com>
2022-10-03 13:06:49 +0530

    BR-2374 - Adding UTs

--- Test/Unit/Helper/DataTest.php
+++ Test/Unit/Helper/DataTest.php
@@ -3,12 +3,19 @@ declare(strict_types=1);
 
 namespace Balancepay\Balancepay\Test\Unit\Helper;
 
+use Balancepay\Balancepay\Model\Config as BalancepayConfig;
+use Balancepay\Balancepay\Model\Request\Factory as RequestFactory;
 use Balancepay\Balancepay\Model\ResourceModel\BalancepayProduct\CollectionFactory as MpProductCollection;
+use Magento\Customer\Api\CustomerRepositoryInterface;
+use Magento\Customer\Model\Session;
 use Magento\Framework\App\Http\Context;
+use Magento\Framework\Pricing\Helper\Data as PricingHelper;
 use Magento\Framework\TestFramework\Unit\Helper\ObjectManager;
 use PHPUnit\Framework\MockObject\MockObject;
+use Magento\Customer\Api\Data\CustomerInterface;
 use PHPUnit\Framework\TestCase;
 use Balancepay\Balancepay\Helper\Data as HelperData;
+use \Balancepay\Balancepay\Model\ResourceModel\BalancepayProduct\Collection;
 
 class DataTest extends TestCase
 {
@@ -29,59 +36,86 @@ class DataTest extends TestCase
             ->disableOriginalConstructor()
             ->onlyMethods([])->getMock();
 
+        $this->session = $this->getMockBuilder(Session::class)
+            ->disableOriginalConstructor()
+            ->onlyMethods([])->getMock();
+
+        $this->collection = $this->getMockBuilder(Collection::class)
+            ->disableOriginalConstructor()
+            ->onlyMethods([])->getMock();
+
+        $this->customerRepositoryInterface = $this->getMockBuilder(CustomerRepositoryInterface::class)
+            ->disableOriginalConstructor()
+            ->onlyMethods([])->getMockForAbstractClass();
+
+        $this->customerInterface = $this->getMockBuilder(CustomerInterface::class)
+            ->disableOriginalConstructor()
+            ->onlyMethods([])->getMockForAbstractClass();
+
+        $this->requestFactory = $this->getMockBuilder(RequestFactory::class)
+            ->disableOriginalConstructor()
+            ->onlyMethods([])->getMock();
+
+        $this->balancepayConfig = $this->getMockBuilder(BalancepayConfig::class)
+            ->disableOriginalConstructor()
+            ->onlyMethods([])->getMock();
+
+        $this->pricingHelper = $this->getMockBuilder(PricingHelper::class)
+            ->disableOriginalConstructor()
+            ->onlyMethods([])->getMock();
+
         $objectManager = new ObjectManager($this);
         $this->testableObject = $objectManager->getObject(HelperData::class, [
-            'requestFactory' => $this->mpProductCollection,
-            'helper' => $this->helperData,
-            'customer' => $this->context,
-            'customerFactory' => $this->customerFactory,
+            'mpProductCollectionFactory' => $this->mpProductCollection,
+            'appContext' => $this->context,
             'customerSession' => $this->session,
             'customerRepositoryInterface' => $this->customerRepositoryInterface,
-            'logger' => $this->loggerInterface,
-            'balancepayConfig' => $this->config
+            'requestFactory' => $this->requestFactory,
+            'balancepayConfig' => $this->balancepayConfig,
+            'pricingHelper' => $this->pricingHelper
         ]);
     }
 
     /**
      * @return void
      */
-    public function testUpdateCustomerBalanceBuyerId()
+    public function testGetBalanceVendors()
     {
-        $this->session->expects($this->any())->method('getCustomer')->willReturn($this->customer);
-        $this->customer->expects($this->any())->method('load')->willReturnSelf();
-        $this->customer->expects($this->any())->method('getDataModel')->willReturn($this->customerInterface);
-        $this->customerInterface->expects($this->any())->method('setCustomAttribute')->willReturnSelf();
-        $this->customer->expects($this->any())->method('updateData')->willReturnSelf();
-        $this->customerFactory->expects($this->any())->method('create')->willReturn($this->customerResource);
-        $this->customerResource->expects($this->any())->method('saveAttribute')->willReturnSelf();
-        $result = $this->testableObject->updateCustomerBalanceBuyerId(
-            'RnGw68WL1qFDKJJJ5Qnnhn38dVrEejcRGdJvA',
-            0
-        );
+        $this->mpProductCollectionFactory->expects($this->any())->method('create')
+            ->willReturn($this->collection);
+        $this->collection->expects($this->any())->method('addFieldToFilter')
+            ->willReturnSelf();
+        $this->collection->expects($this->any())->method('getFirstItem')
+            ->willReturn();
+        $result = $this->testableObject->getBalanceVendors('12');
     }
 
-    public function testGetCustomerBalanceBuyerId()
+    public function testGetCustomerSessionId()
     {
-        $this->session->expects($this->any())->method('getCustomer')->willReturn($this->customer);
-        $this->customer->expects($this->any())->method('getId')->willReturn(5);
-        $this->customerRepositoryInterface->expects($this->any())->method('getById')->willReturn($this->customerInterface);
-        $this->customerInterface->expects($this->any())->method('__toArray')->willReturn([
-            'custom_attributes' => [
-                'buyer_id' => [
-                    'value' => 'byc_34234234jsdf3423'
+        $this->appContext->expects($this->any())->method('getValue')
+            ->willReturn(12);
+        $result = $this->testableObject->getCustomerSessionId();
+    }
+
+    public function testGetBuyerAmountNoBuyerId()
+    {
+        $this->customerSession->expects($this->any())->method('getBuyerId')
+            ->willReturn(0);
+        $this->appContext->expects($this->any())->method('getValue')
+            ->willReturn(12);
+        $this->customerRepositoryInterface->expects($this->any())->method('getById')
+            ->willReturn($this->customerInterface);
+        $this->customerInterface->expects($this->any())->method('__toArray')
+            ->willReturn([
+                'custom_attributes' => [
+                    'buyer_id' => [
+                        'value' => 12
+                    ]
                 ]
-            ]
-        ]);
-        $result = $this->testableObject->getCustomerBalanceBuyerId();
-        $this->assertEquals('byc_34234234jsdf3423', $result);
-    }
-
-    public function testGetCustomerBalanceBuyerIdNoId()
-    {
-        $this->session->expects($this->any())->method('getCustomer')->willReturn($this->customer);
-        $this->customer->expects($this->any())->method('getId')->willReturn('');
-        $result = $this->testableObject->getCustomerBalanceBuyerId();
-        $this->assertNull($result);
+            ]);
+        $this->customerSession->expects($this->any())->method('setBuyerId')
+            ->willReturnSelf();
+        $result = $this->testableObject->getBuyerAmount();
     }
 }
 
