7ef2c2b87fb456fb0f9ea91a79cf3e8e29f4d32d
Omer Gilad <omergilad@gmail.com>
2022-11-29 22:01:43 +0700

    Add source header with 'magento' on all HTTP requests

---
+++ Model/HttpConstants.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Balancepay\Balancepay\Model;
+
+class HttpConstants
+{
+    public const HEADER_CONTENT_TYPE = 'Content-Type';
+    public const HEADER_BALANCE_X_API_KEY = 'x-api-key';
+    public const HEADER_BALANCE_SOURCE = 'source';
+
+    public const CONTENT_TYPE_APPLICATION_JSON = 'application/json';
+    public const BALANCE_SOURCE_MAGENTO = 'magento';
+}
