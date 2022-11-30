694ad29ff5e3cd4a520dfeb692e8a236fedcf8aa
Dhirajkumar Deore <dhirajkumar.41@gmail.com>
2022-11-30 19:15:02 +0530

    BR-3787 - Removed unwanted comment

--- Model/Response/Transactions/Cancel.php
+++ Model/Response/Transactions/Cancel.php
@@ -1,13 +1,4 @@
 <?php
-/**
- * Balance Payments For Magento 2
- * https://www.getbalance.com/
- *
- * @category Balance
- * @package  Balancepay_Balancepay
- * @author   Developer: Pniel Cohen
- * @author   Company: Girit-Interactive (https://www.girit-tech.com/)
- */
 
 namespace Balancepay\Balancepay\Model\Response\Transactions;
 
