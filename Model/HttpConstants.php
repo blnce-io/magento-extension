ba0466d00e357a253e879589d2809c1089bfd607
Raimon Lapuente <raimon@getbalance.com>
2024-05-22 10:20:48 +0200

    fix the string

--- Model/HttpConstants.php
+++ Model/HttpConstants.php
@@ -11,6 +11,5 @@ class HttpConstants
     public const CONTENT_TYPE_APPLICATION_JSON = 'application/json';
     public const BALANCE_SOURCE_MAGENTO = 'magento';
     public const HTTP_USER_AGENT = 'User-Agent';
-    public const HTTP_HARDCODED_USER_AGENT = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Magento/20100101 Firefox/15.0.1
-    ';
+    public const HTTP_HARDCODED_USER_AGENT = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Magento/20100101 Firefox/15.0.1';
 }
